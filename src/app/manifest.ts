4a88d3af1679383637a366ba2e786aa61c7abacc
Jan <jan.hoeck@gmx.net>
2026-07-03 19:18:44 +0200

    Update static exports: switch to `serve` for production, set `dynamic` to `force-static`, unoptimize images, and adjust configs

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Jan Höck — Senior Frontend Entwickler',
